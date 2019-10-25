e8abd109a9da45414c89a3ea1cb908dff0dd930e
Einar Ingebrigtsen <einar@dolittle.com>
2019-10-25 15:46:19 +0200

    Fixing versioning

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export VERSION=$(git tag --sort=-version:refname | head -1)
-docker build --no-cache -f ./Source/Dockerfile -t shipos/timeseries-nmea:$VERSION . --build-arg CONFIGURATION="Release"
+docker build --no-cache -f ./Source/Dockerfile -t shipos/timeseries-nmea . --build-arg CONFIGURATION="Release"
+docker tag shipos/timeseries-nmea shipos/timeseries-nmea::$VERSION
 docker push shipos/timeseries-nmea:$VERSION
