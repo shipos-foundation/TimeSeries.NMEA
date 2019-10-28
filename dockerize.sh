efcf1e29de462267c9656f2f95ac8a5720e6b1a0
Einar Ingebrigtsen <einar@dolittle.com>
2019-10-28 11:39:50 +0100

    Removing config file as this is coming from IoT Edge

--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export VERSION=$(git tag --sort=-version:refname | head -1)
 docker build --no-cache -f ./Source/Dockerfile -t shipos/timeseries-nmea . --build-arg CONFIGURATION="Release"
-docker tag shipos/timeseries-nmea shipos/timeseries-nmea::$VERSION
+docker tag shipos/timeseries-nmea shipos/timeseries-nmea:$VERSION
 docker push shipos/timeseries-nmea:$VERSION
