56bc88430d1d73da9c50f44064923e88f366994a
Einar Ingebrigtsen <einar@dolittle.com>
2019-11-01 17:17:36 +0100

    Removing data

--- dockerize.sh
+++ dockerize.sh
@@ -2,4 +2,5 @@
 export VERSION=$(git tag --sort=-version:refname | head -1)
 docker build --no-cache -f ./Source/Dockerfile -t shipos/timeseries-nmea . --build-arg CONFIGURATION="Release"
 docker tag shipos/timeseries-nmea shipos/timeseries-nmea:$VERSION
+docker push shipos/timeseries-nmea:latest
 docker push shipos/timeseries-nmea:$VERSION
